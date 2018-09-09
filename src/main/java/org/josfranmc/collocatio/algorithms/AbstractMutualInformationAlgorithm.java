c99d626ec092abce68fd86d56f26ad7fc094f5f2
josfranmc <josfranmc@gmail.com>
2018-09-09 22:45:22 +0200

    Actualizar signatura métodos principales

--- src/main/java/org/josfranmc/collocatio/algorithms/AbstractMutualInformationAlgorithm.java
+++ src/main/java/org/josfranmc/collocatio/algorithms/AbstractMutualInformationAlgorithm.java
@@ -5,17 +5,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
+import org.josfranmc.collocatio.triples.TriplesCollection;
 
 /**
  * Encapsula el algoritmo que describe la obtención de colocaciones mediante el cálculo del valor denominado "información mutua".
  * El algoritmo se desarrolla en dos pasos principales:
  * <ul>
- * <li>En el primero se obtiene una colección de tripletas</li>
+ * <li>En el primero se obtiene una colección de tripletas, la cual se define mediante la clase TriplesCollection</li>
  * <li>En el segundo se utilizan éstas tripletas para calcular el valor de información mutua de cada una de ellas</li>
  * </ul>
  * @author Jose Francisco Mena Ceca
  * @version 1.0
  * @see ICollocationAlgorithm
+ * @see TriplesCollection
  */
 public abstract class AbstractMutualInformationAlgorithm implements ICollocationAlgorithm {
 	
@@ -27,20 +29,20 @@ public abstract class AbstractMutualInformationAlgorithm implements ICollocation
 	@Override
 	public void findCollocations() {
 		log.info("USANDO ALGORITMO BASADO EN INFORMACIÓN MUTUA " + getCurrentTime());
-		extractTriples();
-		calculateMutualInformation();
+		TriplesCollection triplesCollection = extractTriples();
+		calculateMutualInformation(triplesCollection);
 		log.info("FIN ALGORITMO BASADO EN INFORMACIÓN MUTUA " + getCurrentTime());
 	}
 	
 	/**
 	 * Obtención de tripletas. Es el primer paso del algoritmo.
 	 */
-	protected abstract void extractTriples();
+	protected abstract TriplesCollection extractTriples();
 	
 	/**
 	 * Cálculo del valor de información mutua de cada tripleta. Es el segundo paso del algoritmo.
 	 */
-	protected abstract void calculateMutualInformation();
+	protected abstract void calculateMutualInformation(TriplesCollection triplesCollection);
 	
 	
 	private String getCurrentTime() {
